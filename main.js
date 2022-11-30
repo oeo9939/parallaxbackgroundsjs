fbe93f5d2f7453d3be6ba0687ceb5358fdf3ae90
Teo <mgttmich@gmail.com>
2022-11-30 22:38:08 +0200

    Add consts layers, gameObjects [], forEach

--- main.js
+++ main.js
@@ -47,7 +47,13 @@ class Layer {
     }
 }
 
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
 
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -59,6 +65,15 @@ function animate() {
     // if (x2 < -2400) x2 = 2400 + x - gameSpeed;
     // else x2 -= gameSpeed;
 
+    // layer4.update();
+    // layer4.draw();
+    // layer5.update();
+    // layer5.draw();
+
+    gameObjects.forEach((object) => {
+        object.update();
+        object.draw();
+    });
     requestAnimationFrame(animate);
 };
 
