0b0396361ca95840fa69c13215a8b6307fea5d93
Teo <mgttmich@gmail.com>
2022-11-30 22:16:18 +0200

    Set animate function, clearRect, drawImage, ifs, requestAni

--- main.js
+++ main.js
@@ -6,13 +6,29 @@ let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "/assets/layer-1.png";
-const backgroundLayer1 = new Image();
+const backgroundLayer2 = new Image();
 backgroundLayer2.src = "/assets/layer-2.png";
-const backgroundLayer1 = new Image();
+const backgroundLayer3 = new Image();
 backgroundLayer3.src = "/assets/layer-3.png";
-const backgroundLayer1 = new Image();
+const backgroundLayer4 = new Image();
 backgroundLayer4.src = "/assets/layer-4.png";
-const backgroundLayer1 = new Image();
+const backgroundLayer5 = new Image();
 backgroundLayer5.src = "/assets/layer-5.png";
 
-function animate()
+let x = 0;
+let x2 = 2400;
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(backgroundLayer4, x, 0);
+    ctx.drawImage(backgroundLayer4, x2, 0);
+
+    if (x < -2400) x = 2400 - gameSpeed;
+    else x -= gameSpeed;
+    if (x2 < -2400) x2 = 2400 - gameSpeed;
+    else x2 -= gameSpeed;
+
+    requestAnimationFrame(animate);
+};
+
+animate();
