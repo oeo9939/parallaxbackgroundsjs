52d5532f1175bc102e9454826df73c49daf81d05
Teo <mgttmich@gmail.com>
2022-11-30 21:42:32 +0200

    Html, css canvas, add consts, backgroundLayers

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # parallaxbackgroundsjs
+
+freeCodeCamp.org
+
+https://www.youtube.com/watch?v=GFO_txvwK_c&ab_channel=freeCodeCamp.org
+
+Project 2: Parallax Backgrounds
+
+bevouliin.com
---
+++ main.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = "/assets/layer-1.png";
+const backgroundLayer1 = new Image();
+backgroundLayer2.src = "/assets/layer-2.png";
+const backgroundLayer1 = new Image();
+backgroundLayer3.src = "/assets/layer-3.png";
+const backgroundLayer1 = new Image();
+backgroundLayer4.src = "/assets/layer-4.png";
+const backgroundLayer1 = new Image();
+backgroundLayer5.src = "/assets/layer-5.png";
+
+function animate()
